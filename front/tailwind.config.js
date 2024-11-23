cf43244b9d98c79eea581d605296312f11178faa
thales0591 <thales0591@gmail.com>
2024-11-23 13:39:40 -0300

    fix color number

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -12,7 +12,7 @@ export default {
         lightRed: '#B03851',
         lightPink: '#EF3353',
         brightOrange: '#F17144',
-        darkYellow: '#F48036'
+        darkYellow: '#F4B036'
       }
     },
   },
