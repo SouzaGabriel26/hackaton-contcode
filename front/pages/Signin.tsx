b651b321235b842ba9eadfa452171983c90fd7cd
*Gustavo <gustavobbacco@gmail.com>
2024-11-23 14:08:47 -0300

    styles: putting focus on the default color

--- front/pages/Signin.tsx
+++ front/pages/Signin.tsx
@@ -18,19 +18,19 @@ const SignIn: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
-        <h1 className="text-2xl font-semibold text-center mb-6">Sign In</h1>
+        <h1 className=" text-darkRed text-2xl font-semibold text-center mb-6">Sign In</h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label
               htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-sm font-medium text-darkRed "
             >
               Email
             </label>
             <input
               type="email"
               id="email"
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-lightRed sm:text-sm"
               placeholder="Digite seu e-mail"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
@@ -40,14 +40,14 @@ const SignIn: React.FC = () => {
           <div className="mb-4">
             <label
               htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-sm font-medium text-darkRed "
             >
               Senha
             </label>
             <input
               type="password"
               id="password"
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-lightRed sm:text-sm"
               placeholder="Digite sua senha"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
@@ -64,7 +64,7 @@ const SignIn: React.FC = () => {
         <p className="mt-4 text-center text-sm text-gray-600">
           Não tem uma conta?{" "}
           <a href="#" className="text-darkRed hover:underline">
-            <Link to="/signon">Registre-se aqui</Link>
+            <Link to="/signup">Registre-se aqui</Link>
           </a>
         </p>
       </div>
