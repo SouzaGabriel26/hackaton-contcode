604fca6961573ec7565929bb4636555090355793
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-11-24 03:30:33 -0300

    feat(`frontend`): adjusts `AuthContext` to fetch user info by token when signed in

--- front/src/context/AuthContext.tsx
+++ front/src/context/AuthContext.tsx
@@ -1,7 +1,18 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { httpClient } from "../lib/httpClient";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  category: {
+    name: string;
+  }
+}
 
 type AuthContextProps = {
   isSignedIn: boolean;
+  user: User | null;
   signIn: (token: string) => void;
   signOut: () => void;
 }
@@ -14,6 +25,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [isSignedIn, setIsSignedIn] = useState(() => {
     return !!localStorage.getItem(accessTokenKey);
   });
+  const [user, setUser] = useState<User | null>(null);
 
   function signIn(token: string) {
     localStorage.setItem(accessTokenKey, token)
@@ -25,8 +37,23 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     setIsSignedIn(false);
   }
 
+  useEffect(() => {
+    if (isSignedIn) {
+      fetchUser();
+    }
+
+    async function fetchUser() {
+      try {
+        const { data } = await httpClient.get<User>('/user/me');
+        setUser(data);
+      } catch {
+        signOut();
+      }
+    }
+  }, [isSignedIn]);
+
   return (
-    <AuthContext.Provider value={{ isSignedIn, signIn, signOut }}>
+    <AuthContext.Provider value={{ isSignedIn, user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   )
--- front/src/lib/httpClient.ts
+++ front/src/lib/httpClient.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
+import { accessTokenKey } from "../context/AuthContext";
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+});
+
+httpClient.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem(accessTokenKey);
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
 });
