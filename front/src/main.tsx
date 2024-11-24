d87698f0777ef88e0b8ffd66e058d55aa1c86329
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-11-23 23:25:46 -0300

    feat(`frontend`): integrates `Signin` and `Signup` pages with the backend

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,10 +2,15 @@ import './App.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
 import App from './App.tsx';
+import { AuthProvider } from './context/AuthContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <AuthProvider>
+      <App />
+      <Toaster position='bottom-right' />
+    </AuthProvider>
+  </StrictMode>
 )
