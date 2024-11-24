1e42023996dcfa0bf4955184bdf6cd794d13ca49
thales0591 <thales0591@gmail.com>
2024-11-24 13:59:38 -0300

    feat: add pizza image

--- front/src/pages/LandingPage.tsx
+++ front/src/pages/LandingPage.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import pizza from "../../public/assets/pizza.png";
 
 const LandingPage = () => {
   return (
     <div>
       <nav className="flex justify-center gap-5 pr-3 pt-3 pb-3 text-white font-bold bg-brightOrange xl:justify-end fixed top-0 w-full z-10 drop-shadow-md">
         <Link to="/signin">Entrar</Link>
-        <Link to="/signup">Criar conta</Link> 
+        <Link to="/signup">Criar conta</Link>
       </nav>
 
       <div className="bg-gray-100 lg:h-[95vh] sm:h-min-screen flex flex-col justify-center items-center pt-20">
@@ -18,6 +19,11 @@ const LandingPage = () => {
             gastronomia. Receba as melhores recomendações e valores
             personalizados para o seu projeto!
           </p>
+          <img
+            src={pizza} 
+            className="w-full max-w-48 h-auto mx-auto mt-6" 
+            alt="pizza image" 
+          />
         </header>
 
         <main className="bg-white shadow-md rounded-lg w-full max-w-3xl p-8 mx-4 sm:mx-0">
@@ -26,9 +32,13 @@ const LandingPage = () => {
           </h2>
           <ol className="list-decimal list-inside text-gray-600 mb-8">
             <li>
-              Preencha os dados básicos do seu projeto, como o nome dele e tipo de comércio.
+              Preencha os dados básicos do seu projeto, como o nome dele e tipo
+              de comércio.
             </li>
-            <li>Informe sua região para receber custos relacionados ao preço local.</li>
+            <li>
+              Informe sua região para receber custos relacionados ao preço
+              local.
+            </li>
             <li>
               Receba um orçamento detalhado com as melhores recomendações e
               custos estimados.
@@ -45,18 +55,20 @@ const LandingPage = () => {
           </ul>
 
           <div className="text-center py-5">
-            <Link to="/signin" className="bg-lightPink text-white font-bold py-3 px-6 rounded-lg hover:bg-lightRed transition">
-            Comece agora!
+            <Link
+              to="/signin"
+              className="bg-lightPink text-white font-bold py-3 px-6 rounded-lg hover:bg-lightRed transition"
+            >
+              Comece agora!
             </Link>
           </div>
         </main>
       </div>
 
       <footer className="text-center py-3 text-gray-500 flex justify-center items-center">
-          <p>
-            &copy; 2024 Seu Orçamento Gastronômico. Todos os direitos
-            reservados.
-          </p>
+        <p>
+          &copy; 2024 Seu Orçamento Gastronômico. Todos os direitos reservados.
+        </p>
       </footer>
     </div>
   );
